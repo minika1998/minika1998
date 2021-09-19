ebd69c980e207ffc76082098f82ef0eae2591e49
minika1998 <91005099+minika1998@users.noreply.github.com>
2021-09-19 13:35:04 +0530

    Add files via upload

---
+++ company.js
@@ -0,0 +1,13 @@
+function validate()
+{
+	var username=document.getElementById('user name').value;
+	var password=document.getElementById('password').value;
+	if (username=="admin"&& password=="user") {
+		alert("login sucessfully");
+		return false;
+	}
+	else
+	{
+		alert("login failed");
+	}
+}
